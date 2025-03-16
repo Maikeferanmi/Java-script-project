5d6b9691380f33a6a0cb459fe9a3bb27628ed404
maike <michealferanmi02@gmail.com>
2025-03-16 16:26:33 +0100

    git Init

---
+++ Html/logout.js
@@ -0,0 +1,12 @@
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+
+const auth = getAuth();
+
+window.logout = function() {
+    auth.signOut().then(() => {
+        alert("Logged out successfully!");
+        window.location.href = "index.html"; // Redirect to sign-in page
+    }).catch((error) => {
+        console.error("Error logging out:", error);
+    });
+}
